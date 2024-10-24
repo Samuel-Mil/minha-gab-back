d05aff5403d5b1b1baa8be3cade75d330342327a
caioo <matossousacaio.cms@gmail.com>
2024-10-24 19:25:31 -0300

    entity gab Request

--- MinhaGab/src/main/java/backendminhagab/example/MinhaGab/models/GabRequest.java
+++ MinhaGab/src/main/java/backendminhagab/example/MinhaGab/models/GabRequest.java
@@ -1,16 +1,16 @@
 package backendminhagab.example.MinhaGab.models;
 
-import backendminhagab.example.MinhaGab.Enums.StatusGab;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -22,23 +22,17 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "gab")
-public class Gab {
+@Table(name = "gabs_requests") //se quiser botar em port
+public class GabRequest {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    private String path;
-
-    @OneToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "patient_id")
     private UserModel user;
-    
-    @Enumerated(EnumType.STRING)
-    private StatusGab statusGab;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "request_id", nullable = false)
-    private GabRequest gabRequest;
+    @OneToMany(mappedBy = "gabRequest", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Gab> listaGabs;
+    
 }
